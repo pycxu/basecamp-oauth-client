a54fc440ea5e3a04662351b864a5ea6211378d3c
Paul XU <pyc.xuu@gmail.com>
2023-07-07 17:36:16 +1000

    init

--- index.js
+++ index.js
@@ -1,35 +1,41 @@
-require('dotenv').config();
-const axios = require('axios');
-const express = require('express');
-const path = require('path');
+require("dotenv").config();
+const axios = require("axios");
+const express = require("express");
+const path = require("path");
 
 const app = express();
 
-app.use(express.static('static'));
+app.use(express.static("static"));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/static/index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/static/index.html"));
 });
 
-app.get('/auth', (req, res) => {
+app.get("/auth", (req, res) => {
+  res.setHeader("User-Agent", process.env.USER_AGENT);
   res.redirect(
-    `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`,
+    `https://launchpad.37signals.com/authorization/new?type=web_server&client_id=${process.env.CLIENT_ID}&redirect_uri=${process.env.CALLBACK_URL}`
   );
 });
 
-app.get('/oauth-callback', ({ query: { code } }, res) => {
-  const body = {
-    client_id: process.env.GITHUB_CLIENT_ID,
-    client_secret: process.env.GITHUB_SECRET,
-    code,
+app.get("/oauth-callback", ({ query: { code } }, res) => {
+  const config = {
+    headers: { "User-Agent": process.env.USER_AGENT },
+    params: {
+      client_id: process.env.CLIENT_ID,
+      client_secret: process.env.CLIENT_SECRET,
+      type: "web_server",
+      code: code,
+      redirect_uri: process.env.CALLBACK_URL,
+    },
   };
-  const opts = { headers: { accept: 'application/json' } };
+  const opts = { headers: { accept: "application/json" } };
   axios
-    .post('https://github.com/login/oauth/access_token', body, opts)
+    .post("https://launchpad.37signals.com/authorization/token", {}, config)
     .then((_res) => _res.data.access_token)
     .then((token) => {
       // eslint-disable-next-line no-console
-      console.log('My token:', token);
+      console.log("Access token:", token);
 
       res.redirect(`/?token=${token}`);
     })
@@ -38,4 +44,4 @@ app.get('/oauth-callback', ({ query: { code } }, res) => {
 
 app.listen(3000);
 // eslint-disable-next-line no-console
-console.log('App listening on port 3000');
+console.log("App listening on port 3000");
--- static/main.js
+++ static/main.js
@@ -1,17 +1,22 @@
 const URL_PARAMS = new URLSearchParams(window.location.search);
-const TOKEN = URL_PARAMS.get('token');
+const TOKEN = URL_PARAMS.get("token");
 
 // Show an element
 const show = (selector) => {
-  document.querySelector(selector).style.display = 'block';
+  document.querySelector(selector).style.display = "block";
 };
 
 // Hide an element
 const hide = (selector) => {
-  document.querySelector(selector).style.display = 'none';
+  document.querySelector(selector).style.display = "none";
+};
+
+const showToken = (selector, token) => {
+  document.querySelector(selector).innerText = token;
 };
 
 if (TOKEN) {
-  hide('.content.unauthorized');
-  show('.content.authorized');
+  hide(".content.unauthorized");
+  show(".content.authorized");
+  showToken(".token", TOKEN);
 }
